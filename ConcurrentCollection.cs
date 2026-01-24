e5e1132cc3e53e0fafe60d6acd3c386e2b2eecfb
Matthias Vill <github@cconstruct.de>
2026-01-24 10:17:41 +0100

    Immutable based concurrent collections

---
+++ ConcurrentCollection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace DotnetPlayground;
+
+public class ConcurrentCollection<T>(ImmutableArray<T> items) : ICollection<T>, IReadOnlyCollection<T>
+{
+    private ImmutableArray<T> _items = items;
+
+    public int Count => _items.Length;
+
+    bool ICollection<T>.IsReadOnly => false;
+
+    public ConcurrentCollection() : this(ImmutableArray<T>.Empty)
+    {
+    }
+
+    public ImmutableArray<T>.Enumerator GetEnumerator() => _items.GetEnumerator();
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>) _items).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _items).GetEnumerator();
+
+    public void Add(T item)
+    {
+        ImmutableInterlocked.Update(ref _items, static (items, item) => items.Add(item), item);
+    }
+
+    public bool AddIfNotContained(T item)
+    {
+        return ImmutableInterlocked.Update(
+            ref _items,
+            static (items, item) => items.Contains(item) ? items : items.Add(item),
+            item);
+    }
+
+    public bool Contains(T item) => _items.Contains(item);
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        _items.CopyTo(array, arrayIndex);
+    }
+
+    public bool Remove(T item)
+    {
+        return ImmutableInterlocked.Update(ref _items, static (items, item) => items.Remove(item), item);
+    }
+
+    public bool RemoveAll(Predicate<T> predicate)
+    {
+        return ImmutableInterlocked.Update(ref _items, static (items, predicate) => items.RemoveAll(predicate), predicate);
+    }
+
+    public void Clear()
+    {
+        ImmutableInterlocked.InterlockedExchange(ref _items, ImmutableArray<T>.Empty);
+    }
+}
---
+++ ConcurrentSet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace DotnetPlayground;
+
+public class ConcurrentSet<T>(ImmutableHashSet<T> items) : ICollection<T>, IReadOnlyCollection<T>
+{
+    private ImmutableHashSet<T> _items = items;
+
+    public int Count => _items.Count;
+
+    bool ICollection<T>.IsReadOnly => false;
+
+    public ConcurrentSet() : this(ImmutableHashSet<T>.Empty)
+    {
+    }
+
+    public ConcurrentSet(IEqualityComparer<T>? comparer) : this(ImmutableHashSet.Create(comparer))
+    {
+    }
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _items).GetEnumerator();
+
+    public void Add(T item)
+    {
+        ImmutableInterlocked.Update(ref _items, static (items, item) => items.Add(item), item);
+    }
+
+    public bool Contains(T item) => _items.Contains(item);
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        ((ICollection<T>) _items).CopyTo(array, arrayIndex);
+    }
+
+    public bool Remove(T item)
+    {
+        return ImmutableInterlocked.Update(ref _items, static (items, item) => items.Remove(item), item);
+    }
+
+    public void Clear()
+    {
+        ImmutableInterlocked.Update(ref _items, static items => items.Clear());
+    }
+}
