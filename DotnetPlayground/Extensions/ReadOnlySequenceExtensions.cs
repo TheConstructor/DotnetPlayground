228cf81d16b8e4342f69e060d3d0d2a6771a39e8
Matthias Vill <github@cconstruct.de>
2026-03-17 19:49:10 +0100

    Replace goto with long ifs

--- DotnetPlayground/Extensions/ReadOnlySequenceExtensions.cs
+++ DotnetPlayground/Extensions/ReadOnlySequenceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Buffers;
+using System.Diagnostics;
 using System.Text;
 
 namespace DotnetPlayground.Extensions;
@@ -37,66 +38,86 @@ public static class ReadOnlySequenceExtensions
                 var startOfNextSegment = startOfCurrentSegment;
                 while (sequence.TryGet(ref startOfNextSegment, out var currentSegment, advance: true))
                 {
+                    // Can we find a complete match inside the current segment?
                     if (currentSegment.Span.IndexOf(value, comparisonType) is var pos and >= 0)
                     {
                         return checked((int) (sequence.GetOffset(startOfCurrentSegment) + pos));
                     }
+                    // Is this the last segment?
                     else if (startOfNextSegment.GetObject() == null)
                     {
                         return -1;
                     }
 
+                    // There is another segment, but there was no complete match inside the current
+                    // => we need at least one Rune from the next segment to form a match.
+                    // We count the Runes in value and then start by looking at one less Rune from the current segment
+                    // to see, if it could be the start of value
                     valueRuneCount ??= value.CountRunes();
                     var remainder = currentSegment.Span.TakeRunesFromEnd(valueRuneCount.Value - 1, out var runesFound);
-                    if (remainder.IsEmpty)
-                    {
-                        goto nextSegment;
-                    }
 
-                    var lastRuneHalved = Rune.DecodeLastFromUtf16(remainder, out _, out var charsConsumed) == OperationStatus.NeedMoreData
-                        && charsConsumed == 1;
-                    if (lastRuneHalved)
+                    if (!remainder.IsEmpty)
                     {
-                        remainder = remainder[..^1];
-                        runesFound--;
-                    }
-
-                    var valueStart = value.TakeRunesFromStart(runesFound, out var valueRunesFound);
-                    if (valueRunesFound != runesFound)
-                    {
-                        goto nextSegment;
-                    }
-
-                    while (!remainder.IsEmpty)
-                    {
-                        if (!remainder.Equals(valueStart, comparisonType))
+                        // It could be, that the current segment ends with the first half of a Rune and the remaining char is in the next segment
+                        var lastRuneHalved = Rune.DecodeLastFromUtf16(remainder, out _, out var charsConsumed)
+                                             == OperationStatus.NeedMoreData
+                                             && charsConsumed == 1;
+                        if (lastRuneHalved)
                         {
-                            remainder = remainder.SkipFirstRune();
-                            valueStart = valueStart.SkipLastRune();
-                            continue;
+                            remainder = remainder[..^1];
+                            runesFound--;
                         }
 
-                        var offsetFromSegmentStart = lastRuneHalved
-                            ? currentSegment.Span.Length - remainder.Length - 1
-                            : currentSegment.Span.Length - remainder.Length;
-                        var sequenceStartingAtI = sequence.Slice(startOfCurrentSegment)
-                            .Slice(offsetFromSegmentStart);
-                        if (sequenceStartingAtI.Length < valueRuneCount.Value)
-                        {
-                            return -1;
-                        }
+                        var valueStart = value.TakeRunesFromStart(runesFound, out var valueRunesFound);
+                        // We counted the Runes in value and runesFound should be less
+                        Debug.Assert(valueRunesFound == runesFound);
 
-                        buffer ??= MemoryPool<char>.Shared.Rent(valueRuneCount.Value * MaxCharsPerRune);
-                        var lengthToCopy = Math.Min(valueRuneCount.Value * MaxCharsPerRune, checked((int)sequenceStartingAtI.Length));
-                        var bufferSpan = buffer.Memory.Span[..lengthToCopy];
-                        sequenceStartingAtI.Slice(0, lengthToCopy).CopyTo(bufferSpan);
-                        if (bufferSpan.TakeRunesFromStart(valueRuneCount.Value, out _).Equals(value, comparisonType))
+                        do
                         {
-                            return checked((int) sequence.GetOffset(startOfCurrentSegment) + offsetFromSegmentStart);
-                        }
+                            // Do the last Runes of this segment match the first Runes of value?
+                            if (!remainder.Equals(valueStart, comparisonType))
+                            {
+                                // No -> retry with one Rune less
+                                remainder = remainder.SkipFirstRune();
+                                valueStart = valueStart.SkipLastRune();
+                                continue;
+                            }
+
+                            // We already matched the start of value, let's see if we can find the rest.
+                            // 1. Get a ReadOnlySpan<char> that starts with the contents of remainder -> sequenceStartingWithRemainder
+                            var offsetFromSegmentStart = lastRuneHalved
+                                ? currentSegment.Span.Length - remainder.Length - 1
+                                : currentSegment.Span.Length - remainder.Length;
+                            var sequenceStartingWithRemainder =
+                                sequence.Slice(startOfCurrentSegment).Slice(offsetFromSegmentStart);
+                            // 2. Check if there are enough chars in sequenceStartingWithRemainder, to be able to contain value
+                            if (sequenceStartingWithRemainder.Length < valueRuneCount.Value)
+                            {
+                                // There is no need to look any further, there are too few characters left in the sequence
+                                return -1;
+                            }
+
+                            // 3. Copy all Runes from sequenceStartingWithRemainder, that could be value, to buffer
+                            buffer ??= MemoryPool<char>.Shared.Rent(valueRuneCount.Value * MaxCharsPerRune);
+                            var lengthToCopy = Math.Min(
+                                valueRuneCount.Value * MaxCharsPerRune,
+                                checked((int) sequenceStartingWithRemainder.Length));
+                            var bufferSpan = buffer.Memory.Span[..lengthToCopy];
+                            sequenceStartingWithRemainder.Slice(0, lengthToCopy).CopyTo(bufferSpan);
+
+                            // 4. Did we find value?
+                            if (bufferSpan.TakeRunesFromStart(valueRuneCount.Value, out _)
+                                .Equals(value, comparisonType))
+                            {
+                                return checked((int) sequence.GetOffset(startOfCurrentSegment) +
+                                               offsetFromSegmentStart);
+                            }
+
+                            // We check only after the first iteration, to handle the lastRuneHalved-situation; in all other cases we already checked this anyway
+                        } while (!remainder.IsEmpty);
                     }
 
-                    nextSegment:
+                    // startOfNextSegment will be updated by sequence.TryGet(..) with the start of the segment after, so let's store the previous value
                     startOfCurrentSegment = startOfNextSegment;
                 }
             }
@@ -104,7 +125,7 @@ public static class ReadOnlySequenceExtensions
             {
                 if (buffer != null)
                 {
-                    buffer.Memory.Span[..value.Length].Clear();
+                    buffer.Memory.Span[..(valueRuneCount.GetValueOrDefault() * MaxCharsPerRune)].Clear();
                     buffer.Dispose();
                 }
             }
@@ -112,7 +133,7 @@ public static class ReadOnlySequenceExtensions
             return -1;
         }
     }
-    
+
     extension<T>(in ReadOnlySequence<T> sequence)
     {
         public bool TryGetSpanWithoutCopy(int start, int length, out ReadOnlySpan<T> span)
